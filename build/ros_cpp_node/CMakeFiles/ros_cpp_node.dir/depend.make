2542c5d8364708fcdf88ee3ac79133ecdffa5b15
chefewq <2405676953@qq.com>
2026-06-21 06:55:11 +0000

    first commit

---
+++ build/ros_cpp_node/CMakeFiles/ros_cpp_node.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ros_cpp_node.
+# This may be replaced when dependencies are built.
---
+++ build/ros_cpp_node/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/azureuser/robot_learn/ros2_learn/node
+# Build directory: /home/azureuser/robot_learn/ros2_learn/build/ros_cpp_node
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
